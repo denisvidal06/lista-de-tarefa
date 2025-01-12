501460d1e118d9af7259369aeb34a74f082c6f98
DenisVidal06 <crfdenis0606@hotmail.com>
2025-01-12 16:37:13 -0400

    finalizei o codigo javascript

--- script.js
+++ script.js
@@ -12,5 +12,42 @@ function listar() {
 
     let textoTarefa = document.createElement('span');
     textoTarefa.textContent = txtTarefa.value;
+    textoTarefa.style.fontWeight = "600";
+    textoTarefa.style.textAlign = 'center';
+
+
+    let item = document.createElement('input');
+    item.setAttribute('type', 'checkbox');
+    item.style.marginRight = '10px';
+
+
+
+    item.addEventListener('change', () => {
+      textoTarefa.classList.toggle('checked', item.checked);
+    })
+
+    let btn = document.createElement('input');
+    btn.setAttribute('type', 'button');
+    btn.value = '🚮';
+    btn.style.backgroundColor = 'red';
+    btn.style.border = "none"
+    btn.style.borderRadius = '5px';
+    btn.style.padding = "5px"
+    btn.style.marginLeft = '10px'
+    btn.style.cursor = 'pointer'
+
+    btn.addEventListener('click', () => {
+      li.remove();
+    })
+
+    li.appendChild(item);
+    li.appendChild(textoTarefa);
+    li.appendChild(btn);
+
+
+    resultado.appendChild(li);
+
+    txtTarefa.value = "";
+    txtTarefa.focus();
   }
 }
